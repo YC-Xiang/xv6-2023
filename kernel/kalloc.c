e963beacb388b124ff52ad5fd4386ddac7265d4a
yucheng_xiang <846735552@qq.com>
2024-12-26 22:30:23 +0800

    Q2 sysinfo

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,14 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64 FreeMemory(void) {
+  struct run *r;
+  int freepage = 0;
+  r = kmem.freelist;
+  while (r) {
+    r = r->next;
+    freepage++;
+  }
+  return freepage * PGSIZE;
+}
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -5,6 +5,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -94,5 +95,18 @@ sys_uptime(void)
 
 uint64 sys_trace(void) {
   argint(0, &myproc()->mask);
+  return 0;
+}
+
+uint64 sys_sysinfo(void) {
+  uint64 addr; // user space struct sysinfo address
+  struct proc *p = myproc();
+  struct sysinfo si;
+  argaddr(0, &addr); // cuz user space store pointer in reg a0, so we can read
+                     // a0 which is the address of struct sysinfo
+  si.freemem = FreeMemory();
+  si.nproc = UsedProcess();
+  if (copyout(p->pagetable, addr, (char *)&si, sizeof(si)) < 0)
+    return -1;
   return 0;
 }
