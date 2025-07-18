da65f0b3611c8173cc1266d3b05fefbafbc85d40
yucheng_xiang <846735552@qq.com>
2025-07-18 19:58:03 +0800

    Q1: Uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,34 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  char stack[STACK_SIZE]; /* the thread's stack */
+  int id;
+  int state;                    /* FREE, RUNNING, RUNNABLE */
+  struct context ctx;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context *store, struct context *load);
               
 void 
 thread_init(void)
@@ -56,10 +76,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch(&t->ctx, &current_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -68,12 +85,16 @@ void
 thread_create(void (*func)())
 {
   struct thread *t;
+  int i = 0;
 
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
+    i++;
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->id = i;
+  t->ctx.ra = (uint64)func;
+  t->ctx.sp = (uint64)t->stack + STACK_SIZE;
 }
 
 void 
